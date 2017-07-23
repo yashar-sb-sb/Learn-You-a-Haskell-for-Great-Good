c09166fca4dfd62e70db7c31989a91988aac9f8b
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-23 12:40:37 +0430

    Access element of list by index

--- 2. Starting Out/list.hs
+++ 2. Starting Out/list.hs
@@ -2,3 +2,5 @@ someNumbers = [4,8,13,18,20]
 eq = [4,8,13,18,20] == 4:8:13:18:20:[]
 someString = 'A':" small cat"
 concatenatedList = [1,2,3]++[4,5,6]
+elementNum = [1,2,3,4,5,6] !! 3
+elementStr = "abcdefghi" !! 0
