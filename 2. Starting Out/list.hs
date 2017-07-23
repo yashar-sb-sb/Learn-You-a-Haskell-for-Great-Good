08b82ae2686768fdb9e65c0c1f13207fb3df6d4a
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-23 14:00:23 +0430

    Use tuples

--- 2. Starting Out/list.hs
+++ 2. Starting Out/list.hs
@@ -48,3 +48,8 @@ length' xs = sum [1 | _ <- xs]
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 xxs = [[1,3,5,2,3,1,2,4,5],[1,2,3,4,5,6,7,8,9],[1,2,4,2,1,6,3,1,3,2,3,6]]
 removeOdd = [ [ x | x <- xs, even x ] | xs <- xxs]
+
+tup = (10,13)
+firstTup = fst tup
+secondTup = snd tup
+zipNums = zip [1..] ["one", "two", "three", "four", "five"]
