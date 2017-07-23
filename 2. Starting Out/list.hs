33b0dded56d0f47dd43c8bbeccbfaced5f0c40b6
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-23 12:52:23 +0430

    Use some more functions on lists

--- 2. Starting Out/list.hs
+++ 2. Starting Out/list.hs
@@ -4,7 +4,19 @@ someString = 'A':" small cat"
 concatenatedList = [1,2,3]++[4,5,6]
 elementNum = [1,2,3,4,5,6] !! 3
 elementStr = "abcdefghi" !! 0
+
 headElem = head someNumbers
 tailList = tail someNumbers
 lastElem = last someNumbers
 initList = init someNumbers
+
+lenSomeNumbers = length someNumbers
+isEmptySomeNumbers = null someNumbers
+reverseSomeNumbers = reverse someNumbers
+first3 = take 3 someNumbers
+dropFirst3TakeRest = drop 3 someNumbers
+maxNum = maximum someNumbers
+minNum = minimum someNumbers
+sumNum = sum someNumbers
+productNum = product someNumbers
+has13 = 13 `elem` someNumbers
