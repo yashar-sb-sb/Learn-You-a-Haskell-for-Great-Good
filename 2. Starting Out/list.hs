bc40967499cefab81ef516ae91a33fe9bdde2b8e
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-23 12:44:19 +0430

    Use head tail last init

--- 2. Starting Out/list.hs
+++ 2. Starting Out/list.hs
@@ -4,3 +4,7 @@ someString = 'A':" small cat"
 concatenatedList = [1,2,3]++[4,5,6]
 elementNum = [1,2,3,4,5,6] !! 3
 elementStr = "abcdefghi" !! 0
+headElem = head someNumbers
+tailList = tail someNumbers
+lastElem = last someNumbers
+initList = init someNumbers
