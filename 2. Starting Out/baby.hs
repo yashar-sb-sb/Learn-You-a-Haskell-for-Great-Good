32ba9648e3d9f60e14b220ac9005369514f3d075
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-23 11:25:20 +0430

    Implement doubleSmallNumber'

--- 2. Starting Out/baby.hs
+++ 2. Starting Out/baby.hs
@@ -4,3 +4,5 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
                         then    x
                         else    x*2
+
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
