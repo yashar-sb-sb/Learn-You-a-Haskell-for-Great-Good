a6350374f1610d1595289dfca67fb67d4ca5d20c
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-25 00:42:35 +0430

    Implement isSorted using Typeclasses

--- 3. Types and Typeclasses/3. Typeclasses.hs
+++ 3. Types and Typeclasses/3. Typeclasses.hs
@@ -3,3 +3,12 @@ elem' _ [] = False
 elem' e (x:xs)
     | e == x = True
     | otherwise = elem' e xs
+
+isSorted :: (Ord a) => [a] -> Bool
+isSorted [] = True
+isSorted [_] = True
+isSorted xs = fIsSorted (<=) xs || fIsSorted (>=) xs
+    where
+        fIsSorted _ [] = True
+        fIsSorted _ [_] = True
+        fIsSorted f (x:y:xs) = x `f` y && (fIsSorted f $y:xs)
