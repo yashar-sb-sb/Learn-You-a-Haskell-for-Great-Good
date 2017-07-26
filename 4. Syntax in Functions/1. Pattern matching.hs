d3d0a6aa7a08a0b1ef929579164fe9d02d56c020
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-26 11:00:03 +0430

    Implement some functions related to factorial

--- 4. Syntax in Functions/1. Pattern matching.hs
+++ 4. Syntax in Functions/1. Pattern matching.hs
@@ -8,3 +8,13 @@ lucky' 7 = True
 lucky' x
     | x `div` 10 > 0 = lucky' (x `mod` 10) && lucky' (x `div` 10)
 lucky' _ = False
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+factorial' :: (Integral a) => a -> a
+factorial' n = product [1..n]
+
+factorials :: [Integer]
+factorials = 1:[a*b | (a, b) <- zip [1..] factorials]
