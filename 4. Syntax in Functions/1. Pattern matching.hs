0eec436f0a2238d9fec093b7b12bc9c3384a2cec
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-26 10:16:59 +0430

    Implement lucky'

--- 4. Syntax in Functions/1. Pattern matching.hs
+++ 4. Syntax in Functions/1. Pattern matching.hs
@@ -1,3 +1,10 @@
 lucky :: (Integral a) => a -> String
 lucky 7 = "LUCKY NUMBER SEVEN!"
 lucky _ = "Sorry, you're out of luck, pal!"
+
+lucky' :: (Integral a) => a -> Bool
+lucky' 4 = True
+lucky' 7 = True
+lucky' x
+    | x `div` 10 > 0 = lucky' (x `mod` 10) && lucky' (x `div` 10)
+lucky' _ = False
