1239580be808d48b6a5b40cf06ce51e25d2bf516
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-27 15:48:06 +0430

    Implement bmiTell'

--- 4. Syntax in Functions/3. Where.hs
+++ 4. Syntax in Functions/3. Where.hs
@@ -5,3 +5,15 @@ bmiTell weight height
     | bmi <= 30.0 = "You're fat! Lose some weight, fatty"
     | otherwise = "You're a whale, congratulations!"
     where bmi = weight / height ^ 2
+
+
+bmiTell' :: (Fractional a, Ord a) => a -> a -> String
+bmiTell' weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. I bet you're ugly!"
+    | bmi <= fat = "You're fat! Lose some weight, fatty"
+    | otherwise = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+          skinny = 18.5
+          normal = 25.0
+          fat = 30.0
