25291e41423787f52c685878cb0858dffcf85b2d
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-27 09:11:31 +0430

    Implement bmiTell' which calculates bmi itself

--- 4. Syntax in Functions/2. Guards.hs
+++ 4. Syntax in Functions/2. Guards.hs
@@ -4,3 +4,11 @@ bmiTell bmi
     | bmi <= 25.0 = "You're supposedly normal. I bet you're ugly!"
     | bmi <= 30.0 = "You're fat! Lose some weight, fatty"
     | otherwise = "You're a whale, congratulations!"
+
+
+bmiTell' :: (Fractional a, Ord a) => a -> a -> String
+bmiTell' weight height
+    | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
+    | weight / height ^ 2 <= 25.0 = "You're supposedly normal. I bet you're ugly!"
+    | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty"
+    | otherwise = "You're a whale, congratulations!"
