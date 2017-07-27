6f4eb1fee75afb0d1d7a50b0e10d2ccf699cdc08
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-27 15:52:42 +0430

    Use tuple in where

--- 4. Syntax in Functions/3. Where.hs
+++ 4. Syntax in Functions/3. Where.hs
@@ -17,3 +17,13 @@ bmiTell' weight height
           skinny = 18.5
           normal = 25.0
           fat = 30.0
+
+
+bmiTell'':: (Fractional a, Ord a) => a -> a -> String
+bmiTell'' weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. I bet you're ugly!"
+    | bmi <= fat = "You're fat! Lose some weight, fatty"
+    | otherwise = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+          (skinny, normal, fat) = (18.5, 25.0, 30.0)
