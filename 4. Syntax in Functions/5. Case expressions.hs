3c0956b853071e5cbff1ca754a9aa03ff12a51f2
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-28 20:04:26 +0430

    Use case expressions to implement head

---
+++ 4. Syntax in Functions/5. Case expressions.hs
@@ -0,0 +1,8 @@
+head' :: [a] -> a
+head' [] = error "No head for empty lists!"
+head' (x:_) = x
+
+
+head'' :: [a] -> a
+head'' xs = case xs of [] -> error "No head for empty lists!"
+                       (x:_) -> x
