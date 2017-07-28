6cffb8f96df909f18bd123f8205485f8e851d891
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-28 15:51:26 +0430

    Use pattern matching in let binding

--- 4. Syntax in Functions/4. Let.hs
+++ 4. Syntax in Functions/4. Let.hs
@@ -13,3 +13,6 @@ squareList xs = let square x = x * x in map square xs
 
 
 multHeyThere = (let a = 100; b = 200; c = 300 in a*b*c, let foo = "Hey "; bar = "there!" in foo ++ bar)
+
+
+number2 = (let (a,b,c) = (1,2,3) in a+b+c) * 100
