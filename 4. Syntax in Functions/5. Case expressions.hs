5583a514a704bfc529a84f4c392b7588dde7730a
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-28 20:08:55 +0430

    Implement describeList

--- 4. Syntax in Functions/5. Case expressions.hs
+++ 4. Syntax in Functions/5. Case expressions.hs
@@ -6,3 +6,9 @@ head' (x:_) = x
 head'' :: [a] -> a
 head'' xs = case xs of [] -> error "No head for empty lists!"
                        (x:_) -> x
+
+
+describeList :: [a] -> String
+describeList xs = "The list is " ++ case xs of [] -> "empty."
+                                               [x] -> "a singleton list."
+                                               xs -> "a longer list."
