255cf5bf8072de00275919e71eb6a3b68383097d
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-28 20:18:07 +0430

    Implement describeList using where

--- 4. Syntax in Functions/5. Case expressions.hs
+++ 4. Syntax in Functions/5. Case expressions.hs
@@ -12,3 +12,11 @@ describeList :: [a] -> String
 describeList xs = "The list is " ++ case xs of [] -> "empty."
                                                [x] -> "a singleton list."
                                                xs -> "a longer list."
+
+
+describeList' :: [a] -> String
+describeList' xs = "The list is " ++ what xs
+    where what [] = "empty."
+          what [x] = "a singleton list."
+          what xs = "a longer list."
+
