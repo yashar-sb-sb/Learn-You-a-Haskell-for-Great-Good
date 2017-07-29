e1d2bece8010a757da31b0e12f271029b823addd
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-29 23:20:02 +0430

    Implement take'

--- 5. Recursion/2. A few more recursive functions.hs
+++ 5. Recursion/2. A few more recursive functions.hs
@@ -2,3 +2,10 @@ replicate' :: (Num i, Ord i) => i -> a -> [a]
 replicate' n x
     | n <= 0 = []
     | otherwise = x:replicate' (n-1) x
+
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n [] = []
+take' n _
+    | n <= 0 = []
+take' n (x:xs) = x:take' (n-1) xs
