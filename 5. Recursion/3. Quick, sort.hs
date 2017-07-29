584f12856494fd6f530964715119acc2e7e9249f
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-29 23:49:45 +0430

    Implement quicksort in a shorter way

--- 5. Recursion/3. Quick, sort.hs
+++ 5. Recursion/3. Quick, sort.hs
@@ -4,3 +4,8 @@ quicksort (x:xs) =
     let smallerSorted = quicksort [a | a <- xs, a <= x]
         biggerSorted = quicksort [a | a <- xs, a > x]
     in smallerSorted ++ [x] ++ biggerSorted
+
+
+qs :: (Ord a) => [a] -> [a]
+qs [] = []
+qs (x:xs) = qs(filter (<x) xs) ++ x:qs(filter (>=x) xs)
