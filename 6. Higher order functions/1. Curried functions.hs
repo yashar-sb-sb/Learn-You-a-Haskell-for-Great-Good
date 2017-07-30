6ea394cb3a3b856c7e08227b3b24601979dd6efc
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-07-30 21:59:48 +0430

    Implement compareWithHundred

--- 6. Higher order functions/1. Curried functions.hs
+++ 6. Higher order functions/1. Curried functions.hs
@@ -2,6 +2,10 @@ compareHundredWith :: (Num a, Ord a) => a -> Ordering
 compareHundredWith = compare 100
 
 
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred = (`compare` 100)
+
+
 divideByTen :: (Fractional a) => a -> a
 divideByTen = (/10)
 
