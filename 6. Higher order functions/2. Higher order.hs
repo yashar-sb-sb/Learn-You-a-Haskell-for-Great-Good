b63fd9fc16c6d2bdfb7113f64c5eff97367303ef
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-08-02 15:43:52 +0430

    Implement zipWith'

--- 6. Higher order functions/2. Higher order.hs
+++ 6. Higher order functions/2. Higher order.hs
@@ -1,2 +1,8 @@
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f(f x)
+
+
+zipWith' :: (a -> b -> z) -> [a] -> [b] -> [z]
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
