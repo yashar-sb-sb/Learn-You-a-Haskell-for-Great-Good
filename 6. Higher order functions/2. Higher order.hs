4e9d7ff0136e39cefd686cd41582251b560e64a3
Yashar Shahi <yashar_sb_sb@yahoo.com>
2017-08-03 12:08:55 +0430

    Implement flip'

--- 6. Higher order functions/2. Higher order.hs
+++ 6. Higher order functions/2. Higher order.hs
@@ -6,3 +6,8 @@ zipWith' :: (a -> b -> z) -> [a] -> [b] -> [z]
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
 zipWith' _ [] _ = []
 zipWith' _ _ [] = []
+
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f = g
+    where g x y = f y x
